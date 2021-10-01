f75a2f761c3bd5561717cca56b3c345509a932bb
Brett Etter <brett.etter@identity.org>
2021-10-01 11:44:31 -0500

    IDCOM-799 Made controller a random pubkey

--- client/test/e2e/did/controller.test.ts
+++ client/test/e2e/did/controller.test.ts
@@ -14,7 +14,7 @@ const { expect } = chai;
 const TRANSACTION_FEE = 5000;
 
 const controller =
-  'did:sol:localnet:4TiH6KXzruQDYjR2KLsdrCDLnkjZhi19yqrEkSdZ2At2';
+  'did:sol:localnet:' + Keypair.generate().publicKey.toBase58();
 
 describe('DID Controller operations', function () {
   this.timeout(60_000);
