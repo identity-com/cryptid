c64bf5f9e4ebb9e44dfff852de98ade3129f9976
Martin Riedel <web@riedel-it.de>
2021-10-06 15:26:15 -0400

    chore: removed functionality that Cryptid does not need.

--- wallet/src/utils/walletProvider/factory.js
+++ wallet/src/utils/walletProvider/factory.js
@@ -1,15 +1,10 @@
 import { LocalStorageWalletProvider } from './localStorage';
-import { LedgerWalletProvider } from './ledger';
 import {SolanaAdapterWalletProvider} from "./adapter";
 
 export class WalletProviderFactory {
   static getProvider(type, args) {
     if (type === 'local') {
       return new LocalStorageWalletProvider(args);
-    }
-
-    if (type === 'ledger') {
-      return new LedgerWalletProvider(args);
     }
 
     if (type === 'adapter') {
