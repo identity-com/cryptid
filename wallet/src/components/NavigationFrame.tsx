717563a5c26ba57dd535f891fbeb9378096cbdb3
Martin Riedel <web@riedel-it.de>
2021-10-11 10:53:09 -0400

    fix: Fixed IdentitySelector misplacement

--- wallet/src/components/NavigationFrame.tsx
+++ wallet/src/components/NavigationFrame.tsx
@@ -99,7 +99,7 @@ function NavigationPanel({ isSignerWindow }: { isSignerWindow: boolean }) {
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between h-32">
+            <div className="flex justify-between h-32 items-center">
               <div className="flex">
                 <div className="flex-shrink-0 flex items-center">
                   <img
