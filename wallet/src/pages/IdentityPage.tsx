8a3da52b981fd051a1af2058c9a89ae3e16be3c8
Martin Riedel <web@riedel-it.de>
2021-10-10 21:13:36 -0400

    feature: Removed legacy details and modal.

--- wallet/src/pages/IdentityPage.tsx
+++ wallet/src/pages/IdentityPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {CryptidDetails} from "../components/Cryptid/CryptidDetails";
-import {CryptidDetails as CryptidDetails2} from "../components/Cryptid/CryptidDetails2";
 
 import {useCryptid} from "../utils/Cryptid/cryptid";
 import WalletList from "../components/Wallet/WalletList";
@@ -22,12 +21,7 @@ export default function IdentityPage() {
           </div>
         </div>
       </div>
-      <div className="py-10">
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          {selectedCryptidAccount &&
-          <CryptidDetails2 cryptidAccount={selectedCryptidAccount}/>}
-        </div>
-      </div>
+
       <div className="py-10">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <WalletList wallets={listWallets()} />
