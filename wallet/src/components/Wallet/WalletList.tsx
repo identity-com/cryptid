13fb931a475f0a03ed462dbe9a28a562151d8b87
Martin Riedel <web@riedel-it.de>
2021-10-11 10:53:48 -0400

    fix: Fixed spelling & Local Key presentation

--- wallet/src/components/Wallet/WalletList.tsx
+++ wallet/src/components/Wallet/WalletList.tsx
@@ -57,7 +57,7 @@ export default function WalletList({ wallets, removeCB }: WalletListInterface) {
                     scope="col"
                     className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
-                    Lokal Key
+                    Local Key
                   </th>
                   <th
                     scope="col"
@@ -88,8 +88,11 @@ export default function WalletList({ wallets, removeCB }: WalletListInterface) {
                       <div className="text-sm text-gray-900">{WalletTypeString[wallet.type]}</div>
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap">
-                      {hasLocalKey && <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                      {hasLocalKey(wallet) && <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
                           Yes
+                        </span>}
+                      {!hasLocalKey(wallet) && <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                          No
                         </span>}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
