ceeaf546567548ab55dbdb1e76b5ce9d744247cb
Martin Riedel <web@riedel-it.de>
2021-10-14 10:40:48 -0400

    feature: Added Letter to Avatarselector. Change Color to light red if controlled.

--- wallet/src/components/selectors/IdentitySelector.tsx
+++ wallet/src/components/selectors/IdentitySelector.tsx
@@ -4,6 +4,7 @@ import AddKeyOrCryptidAccountModal from '../Cryptid/AddKeyOrCryptidAccountModal'
 import { Menu, Transition } from '@headlessui/react';
 import { complement } from 'ramda';
 import {CheckIcon} from "@heroicons/react/outline";
+import AliasAvatar from "../Cryptid/AliasAvatar";
 
 
 const classNames = (...classes) => classes.filter(Boolean).join(' ');
@@ -77,12 +78,11 @@ const IdentitySelector = ({ isSignerWindow }: IdentitySelectorInterface ) => {
         <div>
           <Menu.Button className="max-w-xs bg-white text-gray-400 flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-800">
             <span className="sr-only">Select Identity</span>
-            {/*<UserIcon className="h-6 w-6" aria-hidden="true"/>*/}
-            <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-        <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-        </svg>
-      </span>
+            <span className={classNames(
+              selectedCryptidAccount?.isControlled ? 'bg-red-100' : 'bg-gray-100',
+              'inline-block h-12 w-12 rounded-full overflow-hidden')}>
+              <AliasAvatar alias={selectedCryptidAccount?.alias || '?'} />
+            </span>
           </Menu.Button>
         </div>
         <Transition
