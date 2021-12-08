097030369f5f06e7843c2ce6624afd125e43feff
William Brooks <william@identity.org>
2021-12-08 13:03:15 +0200

    IDCOM-1398 Swap out Program ID on dev stage

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -5,9 +5,11 @@ export const SOL_DID_PROGRAM_ID = new PublicKey(
   'idDa4XeCjVwKcprVAo812coUQbovSZ4kDGJf2sPaBnM'
 );
 
-export const DOA_PROGRAM_ID = new PublicKey(
-  'CFE6uDuLK7Hd9HVQqWkXU3nTmdZUm5KCEtFcEfbSTZLy'
-);
+export const DOA_PROGRAM_ID =
+  process.env.STAGE === 'dev'
+    ? new PublicKey('tcrjc6mfci8bQLmXPfrVw9fJm9Y5tr268tByjSQmSe9')
+    : new PublicKey('CFE6uDuLK7Hd9HVQqWkXU3nTmdZUm5KCEtFcEfbSTZLy');
+
 export const DEFAULT_CLUSTER: ExtendedCluster = 'mainnet-beta';
 
 export const DEFAULT_DID_DOCUMENT_SIZE = 1_500;
