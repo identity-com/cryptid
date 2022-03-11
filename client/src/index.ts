ea50aadb022045b4d52d64fdac644be7d0354333
William Brooks <william@identity.org>
2022-03-11 10:07:35 +0200

    IDCOM-1851 Export JSON serialization functions

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,12 +3,16 @@ import { publicKeyToDid } from './lib/solana/util';
 import { didToDefaultDOASigner } from './lib/util';
 export { ExtendedCluster } from './types/solana';
 export { Cryptid } from './api/cryptid';
+export { JsonSerializedTransaction } from './lib/serialize';
+import { jsonDeserialize, jsonSerialize } from './lib/serialize';
 
 export const build = Builder.build;
 
 export const util = {
   publicKeyToDid,
   didToDefaultDOASigner,
+  jsonDeserialize,
+  jsonSerialize,
 };
 
 // Types exports
