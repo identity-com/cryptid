602b922231f8377d1060ff7139b56d6a4d14ce60
William Brooks <william@identity.org>
2022-04-11 11:55:18 +0200

    CI updates

--- wallet/etc/deploy/deploy-site.sh
+++ wallet/etc/deploy/deploy-site.sh
@@ -4,13 +4,13 @@ set -u
 
 if [ "${STAGE}" == "prod" ]; then
   DISTRIBUTION=E2S4Y0ZV7EAMWM
-  BUCKET=cryptid-identity-com
+  BUCKET=cryptid.identity.com
 elif [ ${STAGE} == "preprod" ]; then
   DISTRIBUTION=???
-  BUCKET=cryptid-preprod-identity-com
+  BUCKET=explorer-preprod.identity.com
 elif [ ${STAGE} == "dev" ]; then
-  DISTRIBUTION=E32M643OD1ONGJ
-  BUCKET=cryptid-dev-identity-com
+  DISTRIBUTION=E1SU4WO39BJJ5E
+  BUCKET=cryptid-dev.identity.com
 fi
 
 npx deploy-aws-s3-cloudfront --non-interactive --react --bucket ${BUCKET} --destination ${STAGE} --distribution ${DISTRIBUTION}
