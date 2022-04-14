94fa80264eda097ad15843b933f65a514d6db917
William Brooks <william@identity.org>
2022-04-14 12:51:05 +0200

    fix: deployment details update

--- wallet/etc/deploy/deploy-site.sh
+++ wallet/etc/deploy/deploy-site.sh
@@ -3,14 +3,14 @@ set -e
 set -u
 
 if [ "${STAGE}" == "prod" ]; then
-  DISTRIBUTION=E2S4Y0ZV7EAMWM
-  BUCKET=cryptid.identity.com
+  DISTRIBUTION=E26KAJ3X9S922B
+  BUCKET=cryptid-identity-com
 elif [ ${STAGE} == "preprod" ]; then
   DISTRIBUTION=???
   BUCKET=explorer-preprod.identity.com
 elif [ ${STAGE} == "dev" ]; then
-  DISTRIBUTION=E1SU4WO39BJJ5E
-  BUCKET=cryptid-dev.identity.com
+  DISTRIBUTION=E32M643OD1ONGJ
+  BUCKET=cryptid-dev-identity-com
 fi
 
 npx deploy-aws-s3-cloudfront --non-interactive --react --bucket ${BUCKET} --destination ${STAGE} --distribution ${DISTRIBUTION}
