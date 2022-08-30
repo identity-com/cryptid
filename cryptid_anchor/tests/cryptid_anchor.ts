39d7059fe7fcb11c620445febb8af5afb5128943
Martin Riedel <web@riedel-it.de>
2022-08-30 16:59:29 -0400

    feature: finished tests

--- cryptid_anchor/tests/cryptid_anchor.ts
+++ cryptid_anchor/tests/cryptid_anchor.ts
@@ -1,8 +1,9 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { CryptidAnchor } from "../target/types/cryptid_anchor";
-import { DidSolIdentifier, ExtendedCluster, DID_SOL_PROGRAM } from "@identity.com/sol-did-client";
+import { DidSolIdentifier, ExtendedCluster, DID_SOL_PROGRAM, DidSolService } from "@identity.com/sol-did-client";
 import { PublicKey, LAMPORTS_PER_SOL, SystemProgram, Keypair } from "@solana/web3.js";
+import chai, { expect } from 'chai';
 
 
 describe("cryptid_anchor", () => {
@@ -30,6 +31,11 @@ describe("cryptid_anchor", () => {
     // Add your test here.
     const did = DidSolIdentifier.create(authority.publicKey, cluster);
     const [didAccount] = await did.dataAccount();
+
+    // Initialize a DID because the generative case is not working yet.
+    const didSolService = await DidSolService.build(did, undefined, authority);
+    await didSolService.initialize(10_000).rpc();
+
     // Cryptid Account
     const [cryptidAccount] = await PublicKey.findProgramAddress(
       [anchor.utils.bytes.utf8.encode("cryptid_account"),
@@ -48,71 +54,38 @@ describe("cryptid_anchor", () => {
       toPubkey: PublicKey.default,
     }).data;
 
-    console.log(transferData);
 
-    // TODO: even that simple case does not serialize client-side.
-    const instruction = await program.methods.directExecute([], [], 0).accounts({
-        cryptidAccount,
-        didProgram: DID_SOL_PROGRAM,
-        did: didAccount,
-        signer: authority.publicKey,
+    const tx = await program.methods.directExecute(Buffer.from([]), [{
+      program_id: 5,
+      accounts: [
+        {
+          key: 0,
+          meta: 3,
+        },
+        {
+          key: 4,
+          meta: 2,
+        }
+        ],
+       data: transferData,
+    }], 0).accounts({
+      cryptidAccount,
+      didProgram: DID_SOL_PROGRAM,
+      did: didAccount,
+      signer: authority.publicKey,
       }
-    ).instruction();
+    ).remainingAccounts([{
+      pubkey: recipient.publicKey,
+      isWritable: true,
+      isSigner: false,
+    },{
+      pubkey: SystemProgram.programId,
+      isWritable: false,
+      isSigner: false,
+    }]).rpc();
 
-    console.log(JSON.stringify(instruction, null, 2));
-
-
-
-    // const instruction = await program.methods.directExecute([], [{
-    //   program_id: 0,
-    //   accounts: [
-    //     {
-    //       key: 0,
-    //       meta: 3,
-    //     },
-    //     {
-    //       key: 4,
-    //       meta: 2,
-    //     }
-    //     ],
-    //    data: Buffer.from(new Uint8Array([1,2,3,4])),
-    // }], 0).accounts({
-    //   cryptidAccount,
-    //   didProgram: DID_SOL_PROGRAM,
-    //   did: didAccount,
-    //   signer: authority.publicKey,
-    //   }
-    // ).remainingAccounts([{
-    //   pubkey: recipient.publicKey,
-    //   isWritable: true,
-    //   isSigner: false,
-    // }]).instruction();
-
-
-    // const tx = await program.methods.directExecute([], [{
-    //   program_id: 0,
-    //   accounts: [
-    //     {
-    //       key: 0,
-    //       meta: 3,
-    //     },
-    //     {
-    //       key: 4,
-    //       meta: 2,
-    //     }
-    //     ],
-    //   // data: [],
-    // }], 0).accounts({
-    //   cryptidAccount,
-    //   didProgram: DID_SOL_PROGRAM,
-    //   did: didAccount,
-    //   signer: authority.publicKey,
-    //   }
-    // ).remainingAccounts([{
-    //   pubkey: recipient.publicKey,
-    //   isWritable: true,
-    //   isSigner: false,
-    // }]).rpc();
+    const lamports = await programProvider.connection.getAccountInfo(cryptidAccount).then(a => a.lamports);
+    expect(lamports).to.equal(LAMPORTS_PER_SOL); // Should have lost 1 SOL
 
     console.log("Your transaction signature", tx);
   });
