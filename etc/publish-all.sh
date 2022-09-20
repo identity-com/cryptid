2569212f0bfb9a5da2b0882d792aa3c50a00a90e
dankelleher <dankelleher@yahoo.com>
2022-09-20 16:37:15 +0200

    (Hacky) script for publishing versions and updating dependencies

---
+++ etc/publish-all.sh
@@ -0,0 +1,37 @@
+
+echo Updating to version $1
+
+set -u
+set -e
+
+#yarn build
+
+echo Publishing cryptid-idl@$1
+(cd packages/client/idl && yarn publish --tag alpha --new-version $1 --no-git-tag-version)
+
+echo Publishing cryptid-core@$1
+(cd packages/client/core \
+  && yarn add @identity.com/cryptid-idl@$1 \
+  && yarn publish --tag alpha --new-version $1 --no-git-tag-version \
+)
+
+echo Publishing cryptid-middleware-check-pass@$1
+(cd packages/client/middleware/checkPass \
+  && yarn add @identity.com/cryptid-core@$1 \
+  && yarn publish --tag alpha --new-version $1 --no-git-tag-version \
+)
+
+echo Publishing cryptid@$1
+(cd packages/client/cryptid \
+  && yarn add @identity.com/cryptid-core@$1 @identity.com/cryptid-middleware-check-pass@$1 \
+  && yarn publish --tag alpha --new-version $1 --no-git-tag-version \
+)
+
+(cd packages/tests \
+  && yarn add @identity.com/cryptid-idl@$1 @identity.com/cryptid@$1 \
+  && yarn add @identity.com/cryptid@$1 --no-git-tag-version \
+)
+
+(cd packages/client/cli \
+  && yarn add @identity.com/cryptid@$1 --no-git-tag-version \
+)
